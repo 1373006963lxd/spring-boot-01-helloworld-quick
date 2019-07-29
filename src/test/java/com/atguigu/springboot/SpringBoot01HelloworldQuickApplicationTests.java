a845b7686d7940f29f8dbff52aa558d45e8d2f86
Your Name <you@example.com>
2019-07-29 20:07:42 +0800

    Initial commit

---
+++ src/test/java/com/atguigu/springboot/SpringBoot01HelloworldQuickApplicationTests.java
@@ -0,0 +1,20 @@
+package com.atguigu.springboot;
+
+import com.atguigu.springboot.bean.Person;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBoot01HelloworldQuickApplicationTests {
+    @Autowired
+    Person person;
+    @Test
+    public void contextLoads() {
+        System.out.println(person);
+    }
+
+}
